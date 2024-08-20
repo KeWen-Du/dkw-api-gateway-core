5c7df7b43f339c6a682e0730daec11577e6b1382
Kewen-Du <1219225197@qq.com>
2024-08-21 07:41:20 +0800

    feat:http请求处理

---
+++ src/main/java/dkw/middleware/gateway/session/SessionChannelInitializer.java
@@ -0,0 +1,22 @@
+package dkw.middleware.gateway.session;
+
+import dkw.middleware.gateway.session.handlers.SessionServerHandler;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.channel.socket.SocketChannel;
+
+public class SessionChannelInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel channel) throws Exception {
+        ChannelPipeline line = channel.pipeline();
+        line.addLast(new HttpRequestDecoder());
+        line.addLast(new HttpResponseEncoder());
+        line.addLast(new HttpObjectAggregator(1024 * 1024));
+        line.addLast(new SessionServerHandler());
+    }
+
+}
